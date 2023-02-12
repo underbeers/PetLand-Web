45e6e75f0d3f36af5a37ccd5b83f20143ff46b8a
RaEzhov <ezhov.mail@yandex.ru>
2023-02-12 20:07:42 +0300

    Add tabs component

---
+++ src/components/Tabs/Tabs.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import styles from './Tabs.module.css';
+
+
+type TabsProps = {children: Array<JSX.Element>};
+
+const Tabs: React.FC<TabsProps> = ({children}) => {
+    return (
+        <div className={styles.tabs}>
+            {children}
+        </div>
+    );
+};
+
+export default Tabs;
